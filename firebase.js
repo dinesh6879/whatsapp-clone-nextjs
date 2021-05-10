7c635fb6fbd9d49a71bdd0a05f6619fe801f4a6c
dinesh6879 <82034304+dinesh6879@users.noreply.github.com>
2021-05-10 23:26:38 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,14 +1,15 @@
 import firebase from 'firebase'
 
-const firebaseConfig = {
-  apiKey: "AIzaSyBROfY8rRT9P_Hc4K4Vyr349NGg_LFG2j0",
-  authDomain: "dinesh-6d772.firebaseapp.com",
-  databaseURL: "https://dinesh-6d772-default-rtdb.firebaseio.com",
-  projectId: "dinesh-6d772",
-  storageBucket: "dinesh-6d772.appspot.com",
-  messagingSenderId: "263967240199",
-  appId: "1:263967240199:web:cd07badb6684067cebbaa2",
-};
+var firebaseConfig = {
+    apiKey: "AIzaSyBROfY8rRT9P_Hc4K4Vyr349NGg_LFG2j0",
+    authDomain: "dinesh-6d772.firebaseapp.com",
+    databaseURL: "https://dinesh-6d772-default-rtdb.firebaseio.com",
+    projectId: "dinesh-6d772",
+    storageBucket: "dinesh-6d772.appspot.com",
+    messagingSenderId: "263967240199",
+    appId: "1:263967240199:web:1a6d9db3127e0f0cebbaa2",
+    measurementId: "G-N4ZJ2YHBZ4"
+  };
 
 const app = !firebase.apps.length ? firebase.initializeApp(firebaseConfig) : firebase.app()
 
